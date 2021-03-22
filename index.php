b1f403f557d07a438be3c9a6a3c97659636f6809
g18marma <g18marma@student.his.se>
2021-03-22 15:29:40 +0100

    Upload image page
    Page for uploading images to databases.

--- index.php
+++ index.php
@@ -1,9 +1,28 @@
-<!DOCTYPE html>
+<!DOCTYPE HTML>
 <html>
     <head>
-        <title>test</title>
+        <meta charset="utf-8">
+        <link rel="stylesheet" type="text/css" href="css/style.css">
+        <title>Application</title>
+
     </head>
     <body>
-        
+        <div class="header">
+            <h1>Upload Image</h1>
+        </div>
+        <div class="image-form">
+            <form method="POST" action="#" enctype="multipart/form-data" class="sendForm">
+                    <div class="upload">
+                        <label for="uploadButton" class="custom-file-upload">
+                            Choose Image
+                        </label>   
+                        <input id="uploadButton" type="file" accept="image/*" name="upload_image"/>
+                         
+                    </div>
+                <div class="submit">
+                    <input id="submitButton" type="submit" name="submit_image" value="Sumbit">
+                </div>
+            </form>
+        </div>
     </body>
 </html>
