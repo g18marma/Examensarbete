61ea57c3bc122dd8754e6c63f7ec6662526786e5
g18marma <g18marma@student.his.se>
2021-03-23 13:47:26 +0100

    Create table in MySQL
    Script for creating a table in MySQL

---
+++ sql/MySQLscript.sql
@@ -0,0 +1,14 @@
+DROP DATABASE IF EXISTS mysql_database;
+CREATE DATABASE mysql_database;
+USE mysql_database;
+
+CREATE TABLE Images(
+ID int AUTO_INCREMENT,
+name varchar(255),
+type varchar(255),
+data mediumblob,
+primary key (ID)
+)engine=innodb;
+
+SELECT * FROM Images;
+
