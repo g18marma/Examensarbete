d65d79658659beee7bacbbacd95ce7145171301e
g18marma <g18marma@student.his.se>
2021-03-25 13:22:05 +0100

    Create table in MariaDB
    Script to create a table in MariaDB

---
+++ sql/MariaDBscript.sql
@@ -0,0 +1,13 @@
+DROP DATABASE IF EXISTS mariadb_database;
+CREATE DATABASE mariadb_database;
+USE mariadb_database;
+
+CREATE TABLE Images(
+ID int AUTO_INCREMENT,
+name varchar(255),
+type varchar(255),
+data mediumblob,
+primary key (ID)
+)engine=innodb;
+
+SELECT * FROM Images;
