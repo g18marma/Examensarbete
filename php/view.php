85b92fc920c86eaa04500d33578a0ef6ecd27123
g18marma <g18marma@student.his.se>
2021-03-25 10:44:37 +0100

    PHP compatible with search form
    Possible to fetch image from data based on serach input

--- php/view.php
+++ php/view.php
@@ -1,10 +1,18 @@
 <?php
 require 'MySQLcon.php';
 
-$stat = $PDO->prepare("SELECT * FROM Images");
-$stat->execute();
-    while ($row = $stat->fetch()){ 
-        echo '<div class="caption"><img src="data:image/jpeg;base64,'.base64_encode($row['data']).'"/></div>';
+// Fetch image from data based on serach input
 
-    }
+if(isset($_POST['search'])){
+
+    $id = $_POST['searchInput'];
+    $query = "SELECT data FROM Images where ID='$id'";
+    $stmt = $PDO->prepare($query);
+    $stmt->execute();
+
+    while ($row = $stmt->fetch()){ 
+        echo '<div class="caption"><img src="data:image/jpeg;base64,'.base64_encode($row['data']).'"/></div>';
+    }   
+}
+
 ?>
