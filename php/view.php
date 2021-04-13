b1b94b9a78d1ad5e082fa624d4ab09b53b8a98b1
g18marma <g18marma@student.his.se>
2021-04-13 16:36:40 +0200

    Display image working for all DBs
    Display image working for all  databases

--- php/view.php
+++ php/view.php
@@ -1,7 +1,7 @@
 <?php
-
 // Fetch image from data based on serach input
 
+//db connection
 if(isset($_POST['search'])){
     
     $hiddenV = $_POST['hiddenV'];
@@ -17,12 +17,24 @@ if(isset($_POST['search'])){
     }
     
     $id = $_POST['searchInput'];
-    $query = "SELECT data FROM Images where ID='$id'";
+    $query = "SELECT data FROM Images where id='$id'";
     $stmt = $PDO->prepare($query);
     $stmt->execute();
 
     while ($row = $stmt->fetch()){ 
-        echo '<div style="margin: auto; margin-top: 50px;" class="caption"><img style= "margin-left: auto; margin-right: auto; display: block;" src="data:image/jpeg;base64,'.base64_encode($row['data']).'"/></div>';
+        $image = $row['data'];
+        if ($hiddenV == 'postgreSQL'){
+            // output buffering.
+            ob_start();
+            //This will normally output the image, but because of ob_start(), it won't.
+            fpassthru($row['data']); 
+            // output above is saved to $contents
+            $contents = ob_get_contents(); 
+            ob_end_clean();
+            $image = $contents;
+        }
+        //display image
+        echo '<div style="margin: auto; margin-top: 50px;" class="caption"><img style= "margin-left: auto; margin-right: auto; display: block;" src="data:image/jpeg;base64,'.base64_encode($image).'"/></div>';  
     }   
 }
 
