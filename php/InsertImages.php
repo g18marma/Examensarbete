79319c09eb0d45fcdac22190c027d921fdcf5358
g18marma <g18marma@student.his.se>
2021-04-19 13:51:42 +0200

    insert Images for all databases
    Images are converted correctly into repective database

--- php/InsertImages.php
+++ php/InsertImages.php
@@ -2,34 +2,31 @@
 
 // Insert images to dabase  
 if(isset($_POST['submit_image'])){
-    $hiddenV = $_POST['hiddenV'];
-    
-    if ($hiddenV == 'mySQL'){
-        require 'MySQLcon.php';
-    }   
-    else if ($hiddenV == 'mariaDB'){
-        require 'MariaDBcon.php';
-    }
-    else {
-        require 'PostgreSQLcon.php'; 
-    }
+    $activeDB = $_POST['hiddenV'];
     $name = $_FILES['upload_image']['name'];
     $type = $_FILES['upload_image']['type'];
     $data = file_get_contents($_FILES['upload_image']['tmp_name']);
-    if ($hiddenV == 'postgreSQL') {
-        $escaped = pg_escape_bytea($data);
-    }
-    $stmt = $PDO->prepare("INSERT INTO Images values(default,?,?,?)");
-    $stmt-> bindParam(1,$name);
-    $stmt-> bindParam(2,$type);
-    if ($hiddenV == 'postgreSQL') {
-        $stmt-> bindParam(3,$escaped);
-    }
-    else {
-        $stmt-> bindParam(3,$data); 
-    }
-    $stmt-> execute();
     
+    if ($activeDB == 'mySQL'){
+        require 'MySQLcon.php';
+    }   
+    else if ($activeDB == 'mariaDB'){
+        require 'MariaDBcon.php';
+    }
+    //pg require data to be converted into hexadecimal representation
+    else {
+        $dbconn = pg_connect("host=localhost dbname=postgresql_database user=postgres password=werwerwer");
+        $escaped = bin2hex( $data );
+        pg_query( "INSERT INTO images (id, name, type, data) VALUES (default, ('$name'), ('$type'), decode('{$escaped}' , 'hex'))" );
+        
+    }
+    if ($activeDB != 'postgreSQL'){
+        $stmt = $PDO->prepare("INSERT INTO Images values(default,?,?,?)");
+        $stmt-> bindParam(1,$name);
+        $stmt-> bindParam(2,$type);
+        $stmt-> bindParam(3,$data);
+        $stmt-> execute();
+    } 
 }
 
 ?>
